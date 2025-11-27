dd23b04da7aee2ee3a2ce29967be6cb307cca604
modeler :D <liam.beeson13@outlook.com>
2025-11-27 11:45:53 +1100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 print("Hello, please input your problem into the input area below")
-input1 = int(input("please enter your first value"))
-input2 = int(input("please enter your second value"))
+input1 = int(input("please enter your first value: "))
+input2 = int(input("please enter your second value: "))
 output = input1 + input2
 print(output)
